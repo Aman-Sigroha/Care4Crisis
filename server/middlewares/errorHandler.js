999686e0a5659a05cae9e3d297dbba1acf4c3fc3
Aman-Sigroha <amansigroha4@gmail.com>
2025-05-13 17:04:31 +0530

    error resolving in packages

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,1 +1,19 @@
- 
+/**
+ * Global error handler middleware
+ */
+const errorHandler = (err, req, res, next) => {
+  // Log the error
+  console.error('Error:', err);
+  
+  // Set the status code
+  const statusCode = err.statusCode || 500;
+  
+  // Send error response
+  res.status(statusCode).json({
+    status: 'error',
+    message: err.message || 'Internal server error',
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+};
+
+module.exports = { errorHandler }; 
